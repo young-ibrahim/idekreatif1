23c7502bf38bb18fb759ea61c45372d25348d7dc
young <youngibrahim86@gmail.com>
2025-01-20 23:00:21 +0700

    memperbarui

--- IdeKreatif-starter-main/kategori.php
+++ IdeKreatif-starter-main/kategori.php
@@ -57,9 +57,52 @@ include '.include/toast_notification.php';
                      </td>
                         </tr>
                         <!-- Modal untuk Hapus Data Kategori -->
-
-                        <!-- Modal untuk Update Data Kategori --> 
-
+                         <div class="modal fade" id="deleteCategory_<?= $category['category_id'];?>" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Hapus Kategori</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="proses_kategori.php" method="POST"></form>
+                                        <div>
+                                            <p>Tindakan ini tidak bisa dibatalkan.</p>
+                                            <input type="hidden" name="catID" value="<?=$category['category_id'];?>">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" name="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         </div>
+                        <!-- Modal untuk Update Data Kategori -->
+                         <div id="editCategory_<?= $category['category_id']; ?>" class="modal fade" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Update Data Kategori</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body"></div>
+                                    <form action="proses_kategory.php" method="POST">
+                                        <!-- Input tersembunyi untuk menyimpan ID kategori --> 
+                                         <input type="hidden" name="catID" value="<?= $category['category_id']; ?>">
+                                         <div class="form-group">
+                                            <label>Nama Kategori</label>
+                                            <!-- Input untuk nama kategori -->
+                                             <input type="text" value="<?= $category['category_name']; ?>" name="category_name" class="form-control">
+                                         </div>
+                                         <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                            <button type="submit" name="update" class="btn btn-warning">Update</button>
+                                         </div>
+                                    </form>
+                                </div>
+                            </div>
+                         </div>
                         <?php endwhile; ?>
                     </tbody>
                 </table>
