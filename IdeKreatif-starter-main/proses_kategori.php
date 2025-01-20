358c85192000e5697393a536165fe040c2e456d3
young <youngibrahim86@gmail.com>
2025-01-20 22:08:03 +0700

    proses

---
+++ IdeKreatif-starter-main/proses_kategori.php
@@ -0,0 +1,34 @@
+<?php
+
+// Menghubungkan ke file konfigurasi database
+include("congfig.php");
+
+// Memulai sesi untuk menyimpan notifikasi
+session_start();
+
+// Proses penambahan kategori baru
+if (isset($_POST['simpan'])){
+    // Mengambil data nama kategori dari form
+    $catefory_name = $_POST['category_name'];
+
+    // Query untuk menambahkan data kategori ke dalam database
+    $query = "INSERT INTO categories (category_name) VALUES ('$catefory_name')";
+    $exec = mysqli_query($conn, $query);
+
+    // Menyimpan notifikasi berhasil atau gagal ke dalam session
+    if ($exec){
+        $_SESSION['notification'] = [
+            'type' => 'primary', // Jenis notifikasi (contoh: primary untuk keberhasilan
+            'message' => 'Kategori berhasil ditambahkan!'
+        ];
+    }else{
+        $_SESSION['notification'] =[
+            'type' => 'danger', // Jenis notifikasi (contoh: danger untuk kegagalan)
+            'message' => 'Gagal menambahkan kategori: ' . mysqli_error($conn)
+        ];
+    }
+
+    // Redirect kembali ke halaman kategori
+    header('Location: kategori.php');
+    exit();
+}
